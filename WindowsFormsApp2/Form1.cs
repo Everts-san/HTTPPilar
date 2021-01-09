0e9315de2192732b574ed053618dfac39c61a50b
Everton Miyabukuro <evertonmiyabukuro@gmail.com>
2021-01-09 14:26:31 -0300

    Commit inicial

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            HttpWebRequestBase httpteste = new HttpWebRequestBase();
+            httpteste.Request("https://nfse.itajai.sc.gov.br/");
+            string html = httpteste.ResponseDataText;
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            HtmlNode nodoCaptcha = doc.DocumentNode.SelectSingleNode("//input[@name='captcha']");
+            string captcha = nodoCaptcha.GetAttributeValue("value", "");
+
+            HtmlNode nodoTentativa = doc.DocumentNode.SelectSingleNode("//input[@name='tentativa']");
+            string tentativa = nodoTentativa.GetAttributeValue("value", "1");
+
+            HtmlNode nodoExecutar = doc.DocumentNode.SelectSingleNode("//input[@name='executar']");
+            string executar = nodoExecutar.GetAttributeValue("value", "entrarNoSistema");
+
+
+            ///////////////////////////////////////////////////
+
+            
+            StreamWriter sw = new StreamWriter(httpteste.RequestDataStream);
+            //75.289.595 % 2F0001 - 46
+            //346346
+            sw.Write("cpf=" + HttpUtility.UrlEncode("75.289.595/0001-46") + "&senha=" + HttpUtility.UrlEncode("346346") + "&exec=Entrar&executar=" + executar + "&captcha=" + HttpUtility.UrlEncode(captcha) + "&tentativa=" + tentativa);
+
+            httpteste.Request("https://nfse.itajai.sc.gov.br/controlador.jsp");
+            html = httpteste.ResponseDataText;
+
+            ///////////////////////////////////////////////////
+            httpteste.Request("https://nfse.itajai.sc.gov.br/jsp/nfse/emitido/lote/listagem.jsp");
+            html = httpteste.ResponseDataText;
+
+            ///////////////////////////////////////////////////
+
+            doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            HtmlNode nodoFormAction = doc.DocumentNode.SelectSingleNode("//input[@name='ACTION_FORM_SUBMETIDO']");
+            string formAction = nodoFormAction.GetAttributeValue("value", "");
+            HtmlNode nodoSQLAnaliticoCript = doc.DocumentNode.SelectSingleNode("//input[@name='NAME_SQL_ANALITICO_CRIPT']");
+            string SQLAnaliticoCript = nodoSQLAnaliticoCript.GetAttributeValue("value", "");
+            HtmlNode nodoSQLSinteticoCript = doc.DocumentNode.SelectSingleNode("//input[@name='NAME_SQL_SINTETICO_CRIPT']");
+            string SQLSinteticoCript = nodoSQLSinteticoCript.GetAttributeValue("value", "");
+
+
+
+            sw = new StreamWriter(httpteste.RequestDataStream);
+            //75.289.595 % 2F0001 - 46
+            //346346
+            //ACTION_FORM_SUBMETIDO=ACTION_FORM_SUBMETIDO&NAME_SQL_ANALITICO_CRIPT=&NAME_SQL_SINTETICO_CRIPT=&dtInicial=01%2F01%2F2021&dtFinal=08%2F01%2F2021&lrp_numero=&lrs_numero=0&NAME_BOTAO_CLICADO=Pesquisar
+            string dataIni = "01/01/2021";
+            string dataFim = "01/08/2021";
+            sw.Write("ACTION_FORM_SUBMETIDO="+ formAction+"&NAME_SQL_ANALITICO_CRIPT=" +SQLAnaliticoCript + "&NAME_SQL_SINTETICO_CRIPT="+ SQLSinteticoCript + "&dtInicial="+ HttpUtility.UrlEncode(dataIni) +"&dtFinal="+ HttpUtility.UrlEncode(dataFim)+"&lrp_numero="+"&lrs_numero="+"0"+"&NAME_BOTAO_CLICADO=Pesquisar");
+
+
+
+              //  "cpf=" + HttpUtility.UrlEncode("75.289.595/0001-46") + "&senha=" + HttpUtility.UrlEncode("346346") + "&exec=Entrar&executar=" + executar + "&captcha=" + HttpUtility.UrlEncode(captcha) + "&tentativa=" + tentativa);
+
+            httpteste.Request("https://nfse.itajai.sc.gov.br/jsp/nfse/emitido/lote/listagem.jsp");
+            html = httpteste.ResponseDataText;
+
+            doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            HtmlNodeCollection nodoTabelaDownload = doc.DocumentNode.SelectSingleNode("//form[@id='form_principal']").SelectSingleNode("//table[@class='tableDados']").SelectNodes("//a");
+
+        }
+    }
+}
---
+++ WindowsFormsApp2/Pilar.HttpWebRequestBase.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+public class HttpWebRequestBase
+{
+    public System.Collections.Generic.Dictionary<HttpRequestHeader, string> Headers { get; }
+    
+    public MemoryStream RequestDataStream { get; }
+
+    public MemoryStream ResponseDataStream { get; }
+
+    public string ResponseDataText { get; private set;  }
+    public HttpStatusCode ResponseStatusCode { get; private set; }
+
+    private CookieContainer sessao { get; set; }
+
+	public HttpWebRequestBase()
+	{
+        this.Headers = new System.Collections.Generic.Dictionary<HttpRequestHeader, string>();
+        this.ResponseDataStream = new MemoryStream();
+        this.RequestDataStream = new MemoryStream();
+        this.ResponseDataText = "";
+        this.sessao = new CookieContainer();
+        this.revertHeadersToDefault();
+    }
+
+    public void Request(string URL)
+    {
+        this.ResponseDataStream.SetLength(0);
+
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(URL);
+        req.CookieContainer = this.sessao;
+        internalBeforeRequest(req);
+
+
+        HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+
+        internalAfterRequest(response);
+
+        // Get the stream associated with the response.
+        Stream receiveStream = response.GetResponseStream();
+
+        receiveStream.CopyTo(ResponseDataStream);
+        ResponseDataStream.Seek(0, 0);
+
+
+        // Pipes the stream to a higher level stream reader with the required encoding format.
+        StreamReader readStream = new StreamReader(ResponseDataStream, Encoding.UTF8);
+
+        this.ResponseDataText = readStream.ReadToEnd();
+        readStream = null;
+
+        ResponseDataStream.Seek(0, 0);
+
+        this.ResponseStatusCode = response.StatusCode;
+
+        foreach(Cookie c in response.Cookies)
+        {
+            sessao.Add(c);
+        }
+
+        //Liberar os recursos
+        response.Close();
+
+        this.RequestDataStream.SetLength(0);
+    }
+
+    protected void internalBeforeRequest(HttpWebRequest requisicao)
+    {
+  //    /*  GET / index.jsp HTTP / 1.1
+//Host: nfse.itajai.sc.gov.br
+//User - Agent: Mozilla / 5.0(Windows NT 10.0; Win64; x64; rv: 84.0) Gecko / 20100101 Firefox / 84.0
+//Accept: text / html,application / xhtml + xml,application / xml; q = 0.9,image / webp,*/*;q=0.8
+//Accept-Language: 
+//Accept-Encoding: gzip, deflate, br
+//Connection: keep-alive
+//Cookie: JSESSIONID=qQ-p9KuD7EsGXyHvQhnNFkcq.undefined
+//Upgrade-Insecure-Requests: 1
+
+        if (this.Headers.Count > 0)
+        {
+            foreach (HttpRequestHeader key in this.Headers.Keys)
+            {
+
+                string valorHeader = "";
+                this.Headers.TryGetValue(key, out valorHeader);
+
+                if (key == HttpRequestHeader.UserAgent)
+                {
+                    requisicao.UserAgent = valorHeader;
+                    continue;
+                }
+                else
+                if (key == HttpRequestHeader.Accept)
+                {
+                    requisicao.Accept = valorHeader;
+                    continue;
+                }
+
+                requisicao.Headers.Set(key, valorHeader);
+            }
+        }
+
+        requisicao.KeepAlive = true;
+
+        if (this.RequestDataStream.Length > 0)
+        {
+            this.RequestDataStream.CopyTo(requisicao.GetRequestStream());
+        }
+    }
+
+    protected void internalAfterRequest(HttpWebResponse resposta)
+    {
+    }
+
+    protected void revertHeadersToDefault()
+    {
+        this.Headers.Clear();
+        this.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla / 5.0(Windows NT 10.0; Win64; x64; rv: 84.0) Gecko / 20100101 Firefox / 84.0");
+        //this.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate, br");
+        this.Headers.Add(HttpRequestHeader.AcceptLanguage, "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3");
+        this.Headers.Add(HttpRequestHeader.Accept, "text / html,application / xhtml + xml,application / xml; q = 0.9,image / webp,*/*;q=0.8");
+    }
+
+	/*-HTTPWebRequestBase.Headers : Dictionary
+         Dicionário com os headers padrões
+      -HTTPWebRequestBase.StreamRequestData : Stream
+         StreamData a ser enviado
+      -HTTPWebRequestBase.StreamResponse : Stream
+         Resposta recebida(stream)
+      -HTTPWebRequestBase.internalBeforeRequest()
+         Faz um processo padrão antes de buscar a requisição, ex: setar viewstate de páginas e setar os headers atuais
+      -HTTPWebRequestBase.Request(string URL)
+         Faz uma requisicao com a URL passada e os headers e streamData enviados
+      -HTTPWebRequestBase.internalAfterRequest()
+         Faz um processo padrão depois buscar a requisição, ex: buscar viewstate de páginas e converter o conteúdo do stream da página de ele não for binário ou zip
+      -HTTPWebRequestBase.RevertHeadersToDefault();
+         Reverte os headers para os padrões da página
+      -HTTPWebRequestBase.Download(string URL, string arquivoDestino)
+         Faz o download de uma URL para o arquivo destino.Cria o arquivo se não existe, sobrescreve se existe*/
+}
